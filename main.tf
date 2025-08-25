6e77bf8bb066fd0fbe9dbe36a0b32bba1b95d01d
vpadegaonkar <vpadegaonkar@yahoo.com>
2025-08-25 19:53:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+# main.tf 
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
+output "random_string" {
+    value = random_string.random.id
+}
